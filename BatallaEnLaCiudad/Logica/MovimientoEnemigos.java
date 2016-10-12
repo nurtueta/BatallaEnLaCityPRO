82779e67bc6406fa69e71b46de68384c14e13cad
BatraChin <guillef92@gmail.com>
2016-10-12 15:21:33 -0300

    ALGORITMO MOVIMIENTO ENEMIGOS

--- BatallaEnLaCiudad/Logica/MovimientoEnemigos.java
+++ BatallaEnLaCiudad/Logica/MovimientoEnemigos.java
@@ -6,6 +6,26 @@ import Grafica.GUI;
 
 public class MovimientoEnemigos extends Movimiento{
 
+	
+	/*
+	 * MEGA ALGORITMO MOVIMIENTO GUILLE
+	 * do{
+direccion = rnd.nextInt(3)+1;
+}while(enemigo.movimientoPosible(direccion))
+cantidadAvance = rnd.nextInt(6)+1;
+
+//ENVIAR LO PROXIMO A LA COLA
+
+MOVER(DIRECCION,ENEMIGO);
+
+//FIN ENVIO A COLA
+
+
+wait(250*enemigo.factorVelocidad());
+	
+	 * 
+	 * 
+	 */
 	public MovimientoEnemigos(GUI miGUI)
 	{
 		super(miGUI);
