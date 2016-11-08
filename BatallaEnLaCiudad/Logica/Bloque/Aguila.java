37bd97c1d6f871b3303571b910525c1cf1279a68
BatraChin <guillef92@gmail.com>
2016-11-08 12:45:07 -0300

    aguila

--- BatallaEnLaCiudad/Logica/Bloque/Aguila.java
+++ BatallaEnLaCiudad/Logica/Bloque/Aguila.java
@@ -10,8 +10,7 @@ public class Aguila extends Celda {
 	public Aguila(int x, int y){
 		
 		super(x,y);
-		ImageIcon fot = new ImageIcon(getClass().getResource("/Imagenes/EmblemaRepublica.jpg"));
-		Icon icono = new ImageIcon(fot.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
+		ImageIcon fot = new ImageIcon(getClass().getResource("/Imagenes/emblemarepublica.png"));		Icon icono = new ImageIcon(fot.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
 		this.setIcon(icono);
 		vida=25;
 	}
