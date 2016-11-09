cc26be4eb145e65ff057dbb0f63843496c8994e3
BatraChin <guillef92@gmail.com>
2016-11-09 15:12:05 -0300

    Imagenes en proceso

--- BatallaEnLaCiudad/Grafica/Inicio.java
+++ BatallaEnLaCiudad/Grafica/Inicio.java
@@ -1,8 +1,15 @@
 package Grafica;
 
 import java.awt.EventQueue;
+import java.awt.Image;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -38,10 +45,25 @@ public class Inicio {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		frame = new JFrame(); 
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		/*
+		 * Insercion de la imagen de FONDO. 
+		 * 
+		 */
+		JLayeredPane fondo= new JLayeredPane();
+		JLabel fotito = new JLabel();
+		fondo.setBounds(frame.getBounds());
+		fotito.setBounds(fondo.getBounds());
+		fondo.add(fotito, new Integer(0));
+		
+		ImageIcon fot = new ImageIcon(getClass().getResource("/Imagenes/Main.jpg"));
+		Icon icono = new ImageIcon(fot.getImage().getScaledInstance(frame.getWidth(), frame.getHeight(), Image.SCALE_DEFAULT));
+		fotito.setIcon(icono);
+		frame.setIconImage(fot.getImage());
 		
 		JButton btnStart = new JButton("Start!");
 		btnStart.addActionListener(new ActionListener() {
